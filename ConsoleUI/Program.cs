67612f94dfc2ba26ab5053b736ce883c90cf6369
Kiril Shchasny <kiril3467@yandex.ru>
2017-07-26 21:56:13 +0300

    Fibonacci numbers generator added

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static GenerateFibonacciLogic.Fibonacci;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var fib in GenerateFibonacci(20))
+            {
+                Console.WriteLine(fib);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GenerateFibonacciLogic/Fibonacci.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateFibonacciLogic
+{
+    public class Fibonacci
+    {
+        /// <summary>
+        /// Generates some count of Fibonacci numbers
+        /// </summary>
+        /// <param name="count">Count of numbers to generate</param>
+        /// <returns>Collection of Fibonacci numbers</returns>
+        public static IEnumerable<int> GenerateFibonacci(int count)
+        {
+            int a = 1;
+            yield return a;
+            int b = 1;
+            yield return b;
+            for (int i = 0; i < count - 2; i++)
+            {
+                yield return a + b;
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+        }
+    }
+}
