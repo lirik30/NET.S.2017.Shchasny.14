78243aefc0b264b65de27938265d652a7303b158
Kiril Shchasny <kiril3467@yandex.ru>
2017-07-30 15:27:01 +0300

    Console 'tests' added

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -5,9 +5,25 @@ using BookLogic;
 
 namespace ConsoleUI
 {
-    public class DigitsComparer : IComparer<int>
+    public struct Point2D
     {
-        public int Compare(int x, int y) => Math.Abs(x).ToString().Length - Math.Abs(y).ToString().Length;
+        public int X { get; }
+        public int Y { get; }
+
+        public Point2D(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString() => $"({X},{Y})";
+    }
+
+    #region custom comparer
+
+    public class IntComparer : IComparer<int>
+    {
+        public int Compare(int x, int y) => Math.Abs(x) - Math.Abs(y);
     }
 
     public class StringComparer : IComparer<string>
@@ -15,39 +31,80 @@ namespace ConsoleUI
         public int Compare(string x, string y) => x.Length - y.Length;
     }
 
-    //public class Point2DComparer : IComparer<Point2D>
-    //{
-    //    public int Compare(Point2D x, Point2D y) => (int)(x.Distance - y.Distance);
-    //}
+    public class Point2DComparer : IComparer<Point2D>
+    {
+        public int Compare(Point2D x, Point2D y) => x.Y - y.Y;
+    }
 
     public class BookComparer : IComparer<Book>
     {
         public int Compare(Book x, Book y) => x.Pages - y.Pages;
     }
 
+    #endregion
 
     class Program
     {
         static void Main(string[] args)
         {
-            var tree = new BinarySearchTree<Book>(new[] {
-                new Book(null, "mike", null, 1),
-                new Book(null, "andy", null, 1),
-                new Book(null, "zeta", null, 1),
-                new Book(null, "paul", null, 1)});
-            
-            foreach (var elem in tree.TraverseInorder())
-            {
-                Console.WriteLine(elem);
-            }
+            var intTree = IntInitializer();
+            var dblTree = StringInitializer();
+            var bookTree = BookInitializer();
+            var pointTree = PointInitializer();
 
-            //Console.WriteLine(tree.Contains(new Book(){Pages = 434}));
+            TraverseTest(intTree);
+            TraverseTest(dblTree);
+            TraverseTest(bookTree);
+            TraverseTest(pointTree);
 
             Console.ReadKey();
         }
+
+        private static void TraverseTest<T>(BinarySearchTree<T> tree)
+        {
+            Console.WriteLine("____START_TRAVERSE____");
+            foreach (var elem in tree.TraverseInorder())
+                Console.WriteLine(elem);
+            Console.WriteLine("_____END_TRAVERSE____");
+        }
+
+        #region Initializers
+        private static BinarySearchTree<int> IntInitializer()
+        {
+            return new BinarySearchTree<int>(new[]
+                    { 0, -15, 20, -3, -2, -5, 17, 22, 4, 19, 16, 0},
+                new IntComparer());
+        }
+
+        private static BinarySearchTree<string> StringInitializer()
+        {
+            return new BinarySearchTree<string>(new[]
+                { "A", "B", "C", "T", "F", "K", "Z", "@", "$", "4", "ab", "ba"});
+        }
+
+        private static BinarySearchTree<Book> BookInitializer()
+        {
+            return new BinarySearchTree<Book>(new[]{
+                    new Book(null, null, null, 1000),
+                    new Book(null, null, null, 2014),
+                    new Book(null, null, null, 1344),
+                    new Book(null, null, null, 443)},
+                new BookComparer());
+        }
+
+        private static BinarySearchTree<Point2D> PointInitializer()
+        {
+            return new BinarySearchTree<Point2D>(new[] {
+                    new Point2D(18, 10),
+                    new Point2D(22, 60),
+                    new Point2D(2, 2),
+                    new Point2D(15, 41)},
+                new Point2DComparer());
+        }
+        #endregion
     }
 
-
+    
 
     
 
